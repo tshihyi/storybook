6d72ae54c7101d06e3f954227b0620c4cc624b1b
shelly <tshihyi@gmail.com>
2018-05-04 22:59:05 +0800

    Update register css

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -6,8 +6,6 @@ import { linkTo } from '@storybook/addon-links'
 import { Button, Welcome } from '@storybook/react/demo'
 import { Timeline } from './src/timeline.jsx'
 import { Register } from './src/register.jsx'
-import './css/timeline.sass'
-import './css/register.sass'
 
 storiesOf("107FWE", module)
   .add("System rules with law", () => <Timeline />)
--- stories/src/timeline.jsx
+++ stories/src/timeline.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../css/timeline.sass'
 
 const data = [{
   date: '77/11/15',
