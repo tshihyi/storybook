d0a46d9ede4999f74311a3d0aa8a62226bcff291
shelly <tshihyi@gmail.com>
2019-03-06 20:12:20 +0800

    add case-register

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -6,7 +6,11 @@ import { linkTo } from '@storybook/addon-links'
 import { Button, Welcome } from '@storybook/react/demo'
 import { Timeline } from './src/timeline.jsx'
 import { Register } from './src/register.jsx'
+import { CaseRegister } from './src/fidr/case-register.jsx'
 
-storiesOf("107FWE", module)
+storiesOf("01FWS", module)
   .add("System rules with law", () => <Timeline />)
   .add("register", () => <Register />)
+
+storiesOf("02FIDR", module)
+  .add("Case Register", () => <CaseRegister />)
