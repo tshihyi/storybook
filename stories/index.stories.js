8341209fe1399f6b477e98566a58a2dcc10b70ec
shelly <tshihyi@gmail.com>
2019-04-13 15:14:55 +0800

    Add documents.jsx

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -8,20 +8,20 @@ import { Timeline } from './src/fws/timeline.jsx'
 import { Register } from './src/fws/register.jsx'
 import { News } from './src/fws/news.jsx'
 import { Resume } from './src/resume/resume.jsx'
+import { Documents } from './src/works/documents.jsx'
 import { IDList } from './src/openfile/idlist.jsx'
 
 
 storiesOf("工作簡歷", module)
   .add("履歷簡表", () => <Resume />)
-  .add("履歷投影片", () => <Resume />)
-storiesOf("系統UI", module)
+storiesOf("系統雛型", module)
+storiesOf("系統分析", module)
+  .add("系統功能雛型", ()=><Documents />)
+  .add("系統分析文件", ()=><Documents />)
+storiesOf("專案管理", module)
+  .add("WBS切割", ()=><Documents />)
+storiesOf("其他作品", module)
   .add("農審辦法", () => <Timeline />)
   .add("註冊", () => <Register />)
   .add("最新公告", () => <News />)
   .add("檔案處理", () => <IDList />)
-storiesOf("系統需求文件", module)
-storiesOf("系統設計文件", module)
-storiesOf("資料庫設計文件", module)
-storiesOf("系統教育訓練", module)
-storiesOf("專案管理文件", module)
-storiesOf("過往其他相關", module)
