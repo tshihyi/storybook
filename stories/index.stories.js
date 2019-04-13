87260b151c9cd915724e5f24d7d0f696773e6f1a
shelly <tshihyi@gmail.com>
2019-04-13 13:28:57 +0800

    Add manager-head.html

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -7,18 +7,21 @@ import { Button, Welcome } from '@storybook/react/demo'
 import { Timeline } from './src/fws/timeline.jsx'
 import { Register } from './src/fws/register.jsx'
 import { News } from './src/fws/news.jsx'
-import { CaseRegister } from './src/fidr/case-register.jsx'
 import { Resume } from './src/resume/resume.jsx'
 import { IDList } from './src/openfile/idlist.jsx'
 
 
-storiesOf("履歷", module)
-  .add("覆歷簡表", () => <Resume />)
-storiesOf("107FWS", module)
+storiesOf("工作簡歷", module)
+  .add("履歷簡表", () => <Resume />)
+  .add("履歷投影片", () => <Resume />)
+storiesOf("系統UI", module)
   .add("農審辦法", () => <Timeline />)
   .add("註冊", () => <Register />)
-storiesOf("108FWS", module)
   .add("最新公告", () => <News />)
   .add("檔案處理", () => <IDList />)
-storiesOf("107FIDR", module)
-  .add("案件登錄", () => <CaseRegister />)
+storiesOf("系統需求文件", module)
+storiesOf("系統設計文件", module)
+storiesOf("資料庫設計文件", module)
+storiesOf("系統教育訓練", module)
+storiesOf("專案管理文件", module)
+storiesOf("過往其他相關", module)
