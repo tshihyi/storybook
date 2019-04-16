f2e409ca639e5a24179bc19d0f0f3296e1c5d571
shelly <tshihyi@gmail.com>
2019-04-16 21:15:51 +0800

    Updrage storybook and fullscreen website

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,24 @@
 import { configure, addParameters } from '@storybook/react';
 
-
+addParameters({
+  options: {
+    /**
+     * show story component as full screen
+     * @type {Boolean}
+     */
+    isFullScreen: true,
+    /**
+     * display panel that shows a list of stories
+     * @type {Boolean}
+     */
+    showNav: true,
+    /**
+     * display panel that shows addon configurations
+     * @type {Boolean}
+     */
+    showPanel: false
+  }
+})
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /.stories.js$/);
