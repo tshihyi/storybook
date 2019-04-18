fea6b4fe5f2661e4c4ef01443d67c0eb93c80e71
shelly <tshihyi@gmail.com>
2019-04-18 19:58:47 +0800

    Update documents

--- stories/src/works/documents.jsx
+++ stories/src/works/documents.jsx
@@ -1,69 +1,85 @@
 import React from 'react'
 import '../../css/documents.sass'
-import imageFileUC1 from '../../img/04-uc-01.png'
+import imageFileUC5 from '../../img/05-fws-data-compare.png'
+import imageFileUC6 from '../../img/06-fidr-case.png'
+import imageFileUC7 from '../../img/07-fidr-meeting.png'
+import imageFileUC10 from '../../img/10-education-sys-operation.png'
+import imageFileUC11 from '../../img/11-e2e-automatic-testing.png'
+
+
+
+
 
 const data = [{
-  name: '系統分析文件',
+  name: '使用者個案文件',
   items: [{
-      item: '01-爭審-案件資料',
-      link: 'https://drive.google.com/file/d/1wNyg6lT426ObSSoeSqjlh5QcAiUx9b0c/view?usp=sharing'
+      item: '1-1-福利-職保批次投保',
+      link: ''
     }, {
-      item: '02-爭審-會議議程管理',
-      link: 'https://drive.google.com/open?id=1i5awxGkXzzwkpIm3z9T2LtmX2Ro3-i5E'
-
-    },{
-      item: '03-福利-資料比對',
+      item: '1-2-福利-資料比對',
       link: 'https://drive.google.com/open?id=13FQscxZLIwAFWaTJ0XMbqZOinKjK-jn1'
     }, {
-      item: '04-福利-實耕者資料管理',
-      link: 'https://drive.google.com/open?id=1GS1OEmL7r9UU0IIsSjZtDiDOCd6wbGDM'
+      item: '2-1-爭審-案件資料',
+      link: 'https://drive.google.com/file/d/1wNyg6lT426ObSSoeSqjlh5QcAiUx9b0c/view?usp=sharing'
+    }, {
+      item: '2-1-爭審-會議議程管理',
+      link: 'https://drive.google.com/open?id=1i5awxGkXzzwkpIm3z9T2LtmX2Ro3-i5E'
     }],
 }, {
   name: '教育訓練文件',
   items: [{
-      item: '教育訓練',
+      item: '1-3-福利-保費功能教育訓練',
       link: 'https://drive.google.com/open?id=120-kjeJicILwSpHYGruuxO84b0ASIZtl'
     }]
 }, {
   name: '簡報類型文件',
   items: [{
-      item: 'E2E自動化測試簡介',
+      item: '1-4-福利-E2E自動化測試簡介',
       link: 'https://drive.google.com/open?id=12Apoeaclz3aqzukL8d1r4wAnAVmq7PYc'
     }]
 }]
 
 const test = () =>
   <div>
-    <div>使用者個案</div>
-      <div className="documents docs-layout ">
+    <div className="docs-titles">使用者個案文件</div>
+    <div className="documents docs-layout">
+      <div>
+        <a href="" target="_blank">
+          <img src={imageFileUC5} />
+          <div className="docs-sub-titles">1-1-福利-職保批次投保</div>
+        </a>
+      </div>
+      <div>
+        <a href="https://drive.google.com/open?id=13FQscxZLIwAFWaTJ0XMbqZOinKjK-jn1" target="_blank">
+          <img src={imageFileUC5} />
+          <div className="docs-sub-titles">1-2-福利-資料比對</div>
+        </a>
+      </div>
+      <div>
+        <a href="https://drive.google.com/file/d/1wNyg6lT426ObSSoeSqjlh5QcAiUx9b0c/view?usp=sharing" target="_blank">
+          <img src={imageFileUC6} />
+          <div className="docs-sub-titles">2-1-爭審-案件資料</div>
+        </a>
+      </div>
+      <div>
+        <a href="https://drive.google.com/open?id=1i5awxGkXzzwkpIm3z9T2LtmX2Ro3-i5E" target="_blank">
+          <img src={imageFileUC7} />
+          <div className="docs-sub-titles">2-1-爭審-會議議程管理</div>
+        </a>
+      </div>
+    </div>
+    <div className="docs-titles">投影片簡報文件</div>
+    <div className="documents docs-layout ">
         <div>
           <a href="https://drive.google.com/file/d/1wNyg6lT426ObSSoeSqjlh5QcAiUx9b0c/view?usp=sharing" target="_blank">
-            <img src={imageFileUC1} width="200" height="250" />
-            <div>爭審-案件資料</div>
+            <img src={imageFileUC10} />
+            <div className="docs-sub-titles">1-3-福利-保費功能教育訓練</div>
           </a>
         </div>
-        <div>
-          <a href="https://drive.google.com/file/d/1wNyg6lT426ObSSoeSqjlh5QcAiUx9b0c/view?usp=sharing" target="_blank">
-            <img src={imageFileUC1} width="200" height="250" />
-            <div>爭審-案件資料</div>
-          </a>
-        </div>
-        <div>
-          <a href="https://drive.google.com/file/d/1wNyg6lT426ObSSoeSqjlh5QcAiUx9b0c/view?usp=sharing" target="_blank">
-            <img src={imageFileUC1} width="200" height="250" />
-            <div>爭審-案件資料</div>
-          </a>
-        </div>
-        <div>
-          <a href="https://drive.google.com/file/d/1wNyg6lT426ObSSoeSqjlh5QcAiUx9b0c/view?usp=sharing" target="_blank">
-            <img src={imageFileUC1} width="200" height="250" />
-            <div>爭審-案件資料</div>
-          </a>
-        </div>
-        <div>
-          <a href="https://drive.google.com/file/d/1wNyg6lT426ObSSoeSqjlh5QcAiUx9b0c/view?usp=sharing" target="_blank">
-            <img src={imageFileUC1} width="200" height="250" />
-            <div>爭審-案件資料</div>
+        <div className="img2">
+          <a href="https://drive.google.com/open?id=12Apoeaclz3aqzukL8d1r4wAnAVmq7PYc" target="_blank">
+            <img src={imageFileUC11} />
+            <div className="docs-sub-titles">1-4-福利-E2E自動化測試簡介</div>
           </a>
         </div>
       </div>
--- stories/src/works/prototypes.jsx
+++ stories/src/works/prototypes.jsx
@@ -1,27 +1,25 @@
 import React from 'react'
 import '../../css/prototypes.sass'
-import imageFiles1 from '../../img/01-prototype-facility.png'
+import imageFiles3 from '../../img/01-protoype-fws.png'
 import imageFiles2 from '../../img/02-prototype-fidr.png'
-import imageFiles3 from '../../img/03-protoype-fws.png'
-
-
+import imageFiles1 from '../../img/03-prototype-facility.png'
 
 const test = () =>
   <div className="prototypes">
     <div className="prototypes thumbnail-container">
-      <div className="prototypes thumbnail-img">
-        <a href="https://facility-lc.firebaseapp.com/" target="_blank">
-          <img src={imageFiles1}/><div className="prototypes thumbnail-title">農糧署-設施補助申請管理系統</div>
+    <div className="prototypes thumbnail-img">
+        <a href="https://tshihyi.github.io/fws-insure/#p=%E8%81%B7%E4%BF%9D%E6%89%B9%E6%AC%A1%E6%8A%95%E4%BF%9D" target="_blank"><img src={imageFiles3}/>
+         <div className="prototypes thumbnail-title">01-農委會-農民福利管理系統</div>
         </a>
       </div>
       <div className="prototypes thumbnail-img">
         <a href="https://tshihyi.github.io/fidsr/#p=%E6%A1%88%E4%BB%B6%E5%AF%A9%E6%A0%B8%E7%AE%A1%E7%90%86%E7%B7%A8%E8%BC%AF" target="_blank">
-          <img src={imageFiles2}/><div className="prototypes thumbnail-title">農委會-農保爭議審議管理系統</div>
+          <img src={imageFiles2}/><div className="prototypes thumbnail-title">02-農委會-農保爭議審議管理系統</div>
         </a>
       </div>
       <div className="prototypes thumbnail-img">
-        <a href="https://tshihyi.github.io/fws-insure/#p=%E8%81%B7%E4%BF%9D%E6%89%B9%E6%AC%A1%E6%8A%95%E4%BF%9D" target="_blank"><img src={imageFiles3}/>
-         <div className="prototypes thumbnail-title">農委會-農民福利管理系統</div>
+        <a href="https://facility-lc.firebaseapp.com/" target="_blank">
+          <img src={imageFiles1}/><div className="prototypes thumbnail-title">03-農糧署-設施補助申請管理系統</div>
         </a>
       </div>
     </div>
